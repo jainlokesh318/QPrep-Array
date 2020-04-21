b974a28133306cc989fafab29ddf604cde14a48a
Kiran Suresh <jainlokesh318@gmail.com>
2020-04-21 22:43:18 +0530

    Corrected input for cpp Median problem

--- MedianOfTwoSortedArrays/MedianOfTwoSortedArrays.cpp
+++ MedianOfTwoSortedArrays/MedianOfTwoSortedArrays.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 
 // Implement your solution here
-double findMedianOfTwoSortedArrays(vector<int>& nums1, vector<int>& nums2) { 
+double findMedianOfTwoSortedArrays(vector<int>& nums1, vector<int>& nums2, int size1, int size2) { 
 
 	return 0.0;
 }
 
 int main()
 {
-	int m, n;
+	int m, n, q, size1, size2;
 	cin >> m;
 	vector<int>nums1(m);
 	for(int i = 0 ; i < m ; i++) {
@@ -20,7 +20,12 @@ int main()
 	for(int i = 0 ; i < n ; i++) {
 		cin >> nums2[i];
 	}
-	double answer = findMedianOfTwoSortedArrays(nums1, nums2);
-	cout << answer << "\n";
+	cin >> q;
+        for (int i = 0; i < q ; i++) {
+	    cin >> size1;
+	    cin >> size2;
+	    double answer = findMedianOfTwoSortedArrays(nums1, nums2, size1, size2);
+	    cout << answer << "\n";
+	}
 	return 0;
 }
