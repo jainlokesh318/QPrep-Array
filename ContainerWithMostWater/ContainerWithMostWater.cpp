1683b0dc5c6a7c7d03dd67e74d9cd93cfba50e5a
jainlokesh318 <jainlokesh318@gmail.com>
2020-04-29 11:40:39 +0000

    [LocalTest]:[ContainerWithMostWater]:[c++]

--- ContainerWithMostWater/ContainerWithMostWater.cpp
+++ ContainerWithMostWater/ContainerWithMostWater.cpp
@@ -6,12 +6,21 @@ using namespace std;
 
 class ContainerWithMostWater {
   public:
-    int maxArea(vector<int>& height) {
-        int answer = 0;
-        // Your implementation goes here
-	
-        return answer;
+    
+    int maxArea(vector<int> &nums)
+{
+    int ans = INT_MIN;
+    for(int i = 0; i < nums.size()-1; i++)
+    {
+        int tower = INT_MIN;
+        for(int j = i+1; j < nums.size(); j++)
+        {
+            int curr = min(nums[i], nums[j])*(j-i);
+            ans = max(ans, curr);
+        }
     }
+    return ans;
+}
 };
 
 int main() {
