8ab360db43dca28505c0c6d3603313535c0b5aa8
jainlokesh318 <jainlokesh318@gmail.com>
2020-04-29 06:52:56 +0000

    [LocalTest]:[ThreeSum]:[c++]

--- ThreeSum/ThreeSum.cpp
+++ ThreeSum/ThreeSum.cpp
@@ -7,9 +7,49 @@ using namespace std;
 class ThreeSum {
 public:
     // Implement your solution here	
+
+    void twoSum(vector<int> nums, int req, int fp, set<vector<int>> &res)
+{
+    int sp = nums.size()-1;
+    int cur;
+    while(fp < sp)
+    {
+        cur = nums[fp] + nums[sp];
+        if(cur == req)
+        {
+     //       cout << "\t" << -req << " " << nums[fp] << " " << nums[sp] << "\n";
+            vector<int> trip = {-req, nums[fp], nums[sp]};
+            sort(trip.begin(), trip.end());
+            res.insert(trip);
+            fp++;
+            sp--;
+            while(nums[fp] == nums[fp-1] && fp < sp)
+                fp++;
+            while(nums[sp] == nums[sp+1] && fp < sp)
+                sp++;
+        }
+        else if(cur < req)
+            fp++;
+        else
+            sp++;
+    }
+}
+
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> ans;
+        
+        set<vector<int>> res;
+        for(int i = 0; i < (int)nums.size()-2; i++)
+        {
+            // cout << "For " << i << "\n";
+            if(i > 0 && nums[i] == nums[i-1])
+                continue;
+            twoSum(nums, -nums[i], i+1, res);
+        }
 
+        for(vector<int> x : res)
+            ans.push_back(x);
+            
         return ans;
     }
 };
