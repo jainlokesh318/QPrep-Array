f69bde27a0f3655afb2c638ae9b2a838ed88181c
jainlokesh318 <jainlokesh318@gmail.com>
2020-04-30 14:52:56 +0000

    [LocalTest]:[FruitIntoBaskets]:[c++]

--- FruitIntoBaskets/FruitIntoBaskets.cpp
+++ FruitIntoBaskets/FruitIntoBaskets.cpp
@@ -2,10 +2,35 @@
 using namespace std;
 
 // Implement your solution here
-int totalFruit(vector<int>& tree) {
-	int ans = 0;
+int totalFruit(vector<int>& s) {
+	int k = 2;
+	int n = s.size();
+    if(k == 0 || n == 0)
+        return 0;
 
-	return ans;
+    int l = 0, r = 0, ans = 0, cnt = 0;
+    vector<int> mapi(256, 0);
+
+    for(; r < n; r++)
+    {
+   // cout << "start of window " << sp << " end of window" << ep << "\n";
+        if(mapi[s[r]] == 0)
+            cnt++;
+        mapi[s[r]]++;
+
+      //      cout << "size = " << char_list.size() << "\n";
+        while(cnt > k)
+        {
+        //    cout << "\tok i am here ";
+            mapi[s[l]]--;
+          //  cout << itr->first << " " << itr->second << "\n";
+            if(mapi[s[l]] == 0) cnt--;
+            l++;
+        }
+        ans = max(ans, r-l+1);
+    }
+
+    return ans;
 }
 
 int main()
