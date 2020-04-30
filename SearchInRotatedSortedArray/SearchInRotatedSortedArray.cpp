8efa6ae20ef35c1d6f74370525d84895217a365f
jainlokesh318 <jainlokesh318@gmail.com>
2020-04-30 19:10:38 +0000

    [LocalTest]:[SearchInRotatedSortedArray]:[c++]

--- SearchInRotatedSortedArray/SearchInRotatedSortedArray.cpp
+++ SearchInRotatedSortedArray/SearchInRotatedSortedArray.cpp
@@ -9,6 +9,29 @@ class SearchInRotatedSortedArray {
     // Implement your solution here	  
     int search(vector<int>& nums, int target) {
 
+         int first = 0, last = nums.size()-1;
+        while(first <= last)
+        {
+            int mid = first + (last - first) / 2;
+            
+            if(nums[mid] == target)
+                return mid;
+            
+            if(nums[first] <= nums[mid])
+            {
+                if(target >= nums[first] && target < nums[mid])
+                    last = mid-1;
+                else
+                    first = mid+1;
+            }
+            else
+            {
+                if(target > nums[mid] && target <= nums[last])
+                    first = mid+1;
+                else
+                    last = mid-1;
+            }
+        }
         return -1;
     }
 };
